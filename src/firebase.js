6810b08920a2e86291ad8a5fbb14857d2cf0f531
WarriorOFlonelinesS <ydneprovskiy@gmail.com>
2024-02-09 18:49:10 +0200

    Add Redux Saga Firebase to the project.

--- src/firebase.js
+++ src/firebase.js
@@ -1,23 +1,26 @@
 // Import the functions you need from the SDKs you need
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/firestore';
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
 import { getAuth } from "firebase/auth";
+import "@firebase/firestore";
+import ReduxSagaFirebase from "redux-saga-firebase";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBAWQryCi26Y--Ep7P_KKi2xUXGGnAMZJ8",
-    authDomain: "chat-quiz-app-b7d98.firebaseapp.com",
-    projectId: "chat-quiz-app-b7d98",
-    storageBucket: "chat-quiz-app-b7d98.appspot.com",
-    messagingSenderId: "584339501969",
-    appId: "1:584339501969:web:4ed0639eda9999a6cb6030"
-  };
-  
+  apiKey: "AIzaSyBAWQryCi26Y--Ep7P_KKi2xUXGGnAMZJ8",
+  authDomain: "chat-quiz-app-b7d98.firebaseapp.com",
+  projectId: "chat-quiz-app-b7d98",
+  storageBucket: "chat-quiz-app-b7d98.appspot.com",
+  messagingSenderId: "584339501969",
+  appId: "1:584339501969:web:4ed0639eda9999a6cb6030",
+};
 
 // Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
 export const db = app.firestore();
 export const auth = getAuth(app);
+
+export const rsf = new ReduxSagaFirebase(firebaseConfig);
--- src/redux/sagas/sagas.js
+++ src/redux/sagas/sagas.js
@@ -1,12 +1,29 @@
-import { call, put } from "redux-saga/effects";
-import { db } from "../../firebase";
+import { put } from "redux-saga/effects";
 import { getMessageApi } from "./getMessage";
-import { getMessagesSuccess } from "../messages-slice";
-
+import { getMessagesFailure, getMessagesSuccess } from "../messages-slice";
+import { call } from "redux-saga/effects";
+import { rsf } from "../../firebase"; // Подставьте соответствующий импорт Firebase
+import { actions } from "redux-saga-firebase"; // Импортируйте необходимые действия Firestore
 
 export function* getMessagesSaga() {
- const response = yield getMessageApi()
+  //  const response = yield getMessageApi()
+  //  yield  put( getMessagesSuccess(response))
+      try {
+        const snapshot = yield call(rsf.firestore.getCollection, "messages"); // Чтение коллекции сообщений из Firestore
+        const messages = {};
+        snapshot.forEach((doc) => {
+          messages[doc.id] = doc.data(); // Преобразование документов Firestore в объект сообщений
+        });
+        console.log(messages)
+        // Обработка полученных сообщений здесь, например, отправка их в стор Redux
+        yield put(getMessagesSuccess(messages)); // Отправка сообщений в стор Redux
+      } catch (error) {
+        // Обработка ошибок здесь
+        console.error("Error fetching messages:", error);
+        yield put(getMessagesFailure(error)); // Отправка ошибки в стор Redux
+      }
+    }
 
- yield  put( getMessagesSuccess(response))
-}
-    // Виконайте потрібні дії з отриманими даними
+    // Делайте с messages то, что вам необходимо здесь
+  
+// Виконайте потрібні дії з отриманими даними
