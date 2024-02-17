0c58f64e349692b05b08a615532f455ecf184d5e
Yaroslav Gorlenko <98014616+WarriorOFlonelinesS@users.noreply.github.com>
2024-02-17 14:21:27 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBAWQryCi26Y--Ep7P_KKi2xUXGGnAMZJ8",
-    authDomain: "chat-quiz-app-b7d98.firebaseapp.com",
-    projectId: "chat-quiz-app-b7d98",
-    storageBucket: "chat-quiz-app-b7d98.appspot.com",
-    messagingSenderId: "584339501969",
-    appId: "1:584339501969:web:4ed0639eda9999a6cb6030"
-  };
-  
+#########################################
+  YOUR CONFIGURATION
+#########################################
+};
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+const app = firebase.initializeApp(firebaseConfig);
+export const db = app.firestore();
